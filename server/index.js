9792b647e0ecf655bc1b7eb19dc0438206f7b9bb
oleksii-shepel <oleksii.shepel@outlook.com>
2022-10-26 02:37:12 +0300

    changed cors rules

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(cookieSession({ name: 'session', keys: ['key1', 'key2'] }));
 //app.use(cors());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin','*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
     next();
