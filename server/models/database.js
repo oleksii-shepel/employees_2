b2c63dbbf2248b301538af1d6889165644be7bf4
oleksii-shepel <oleksii.shepel@outlook.com>
2022-10-26 00:24:49 +0300

    changed postgres address

--- server/models/database.js
+++ server/models/database.js
@@ -4,7 +4,15 @@ import dotenv from 'dotenv'
 dotenv.config({ path: './config.env' });
 
 let database_url = process.env.DATABASE_URL || "postgres://username:password@postgres:5432/db";
-export const sequelize = new Sequelize(database_url);
+export const sequelize = new Sequelize(database_url, {
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      } 
+    }});
+
 console.log(database_url);
 
 import user2 from "./user.js";
